f436b3e93cbebd865ec0c6c3d078c3180b1f9e94
Brian May <brian@linuxpenguins.xyz>
2026-02-05 17:45:51 +1100

    Replace poetry with uv

--- flake.nix
+++ flake.nix
@@ -1,11 +1,28 @@
 {
   description = "Time tracking application";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-  inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -13,22 +30,98 @@
       self,
       nixpkgs,
       flake-utils,
-      poetry2nix,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          time-tracking = mkPoetryApplication { projectDir = self; };
-          default = self.packages.${system}.time-tracking;
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python312;
+
+        # Load the uv workspace & overlay
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+        # Create package overlay from workspace.
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "sdist";
         };
 
-        devShells.default = pkgs.mkShell { packages = [ poetry2nix.packages.${system}.poetry ]; };
+        # Extend generated overlay with build fixups
+        #
+        # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
+        # This is an additional overlay implementing build fixups.
+        # See:
+        # - https://pyproject-nix.github.io/uv2nix/FAQ.html
+        pyprojectOverrides =
+          final: prev:
+          # Implement build fixups here.
+          # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
+          # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
+          let
+            inherit (final) resolveBuildSystem;
+            inherit (builtins) mapAttrs;
+
+            # Build system dependencies specified in the shape expected by resolveBuildSystem
+            # The empty lists below are lists of optional dependencies.
+            #
+            # A package `foo` with specification written as:
+            # `setuptools-scm[toml]` in pyproject.toml would be written as
+            # `foo.setuptools-scm = [ "toml" ]` in Nix
+            buildSystemOverrides = {
+              ibis.setuptools = [ ];
+              pyyaml.setuptools = [ ];
+              docutils.flit-core = [ ];
+            };
+
+          in
+          mapAttrs (
+            name: spec:
+            prev.${name}.overrideAttrs (old: {
+              nativeBuildInputs = old.nativeBuildInputs ++ resolveBuildSystem spec;
+            })
+          ) buildSystemOverrides;
+
+        # Build a Python package set from uv.lock
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              nixpkgs.lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                overlay
+                pyprojectOverrides
+              ]
+            );
+
+        # Create a venv with all locked deps
+        appEnv = pythonSet.mkVirtualEnv "time-tracking" workspace.deps.default;
+
+      in
+      {
+        # The app packageible as a Nix build
+        packages.timeTracking = appEnv;
+
+        # Default package for `nix build`
+        packages.default = self.packages.${system}.timeTracking;
+
+        # Dev shell includes the venv and uv CLI
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            # appEnv
+            pkgs.uv
+            python
+          ];
+
+          shellHook = ''
+            # Activate the Python venv
+            # source $appEnv}/bin/activate
+            # export UV_NO_SYNC=1
+            # export UV_PYTHON=${python}/bin/python
+          '';
+        };
       }
     );
 }
