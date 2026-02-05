eb8c9b2886584409c883844623882b4af6c1f6c6
Brian May <brian@linuxpenguins.xyz>
2026-02-05 16:58:58 +1100

    fix: update nix flakes

--- flake.nix
+++ flake.nix
@@ -2,25 +2,33 @@
   description = "Time tracking application";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      poetry2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
         inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         packages = {
           time-tracking = mkPoetryApplication { projectDir = self; };
           default = self.packages.${system}.time-tracking;
         };
 
-        devShells.default =
-          pkgs.mkShell { packages = [ poetry2nix.packages.${system}.poetry ]; };
-      });
+        devShells.default = pkgs.mkShell { packages = [ poetry2nix.packages.${system}.poetry ]; };
+      }
+    );
 }
